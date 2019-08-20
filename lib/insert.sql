c1aef93ff7853ae8a87b50b013dc3e0d56b3cedb
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-20 13:01:18 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ("Mr. Chocolate", 12, "Male");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 8, "Male");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 5, "Female");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 15, "Male");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 9, "Female");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 12, "Male");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 3, "Female");
+INSERT INTO bears (name, age, gender) VALUES (Null, Null, Null);
